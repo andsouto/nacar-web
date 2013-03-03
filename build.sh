98d7b2e6cac93d7d37f589f22d4fa6bdf8d1f8ce
Andrés Souto <kai670@gmail.com>
2013-03-03 22:24:03 +0100

    Lots of changes. First public version

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 rm -r web
 mkdir web
-recess --compile src/less/nacar.less > build/nacar.css
+#recess --compile src/less/nacar.less > web/nacar.css
+recess --compress src/less/nacar.less > web/nacar.css
 jade src/jade/ --out web -P
-mkdir build/img
+mkdir web/img
 cp src/img/logo.svg web/img/
+cp src/img/bg-red.png web/img/
+cp src/img/daisy.png web/img/
+cp src/img/daisy-bg.jpg web/img/
+cp -r src/img/gallery web/img/
+
