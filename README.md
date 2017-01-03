dee7a922ba5158615358a90382fc63dd4b0fca09
Andrés Souto <kai670@gmail.com>
2017-01-03 10:20:11 +0100

    Add a service worker, precomputed gzip compression and small improvements

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 -   Probar a cambiar todo el color en hover y en focus en los botone
--   En novedades poner a la derecha de Facebook una galeria de imágenes
+
+
+-   Mirar todo el tema de buscadores web. Que añadir para que Google y bing saquen bien la entradilla, etc.
+
+    <meta name="description" itemprop="description" content="Ofrecemos soluciones integrales de comunicación tanto a pequeñas y medianas empresas como a grandes corporaciones principalmente en Gipuzkoa, Bizkaia y Araba." />
+
+    <meta name="keywords" itemprop="keywords" content="soluciones comunicacion gipuzkoa,soluciones comunicación gipuzkoa,servicios comunicacion gipuzkoa,servicios comunicación gipuzkoa,soluciones comunicacion bizkaia,soluciones comunicación bizkaia,servicios comunicacion bizkaia,servicios comunicación bizkaia" />
+    <meta name="robots" content="index,follow,noodp,noydir" />
+
+
+- Revisar tamaño de los iconos favicon, IE? https://en.wikipedia.org/wiki/Favicon
+
+- Comprobar compatibilidad en IE. Arreglar los prefixes CSS y añadir mensaje para los IE antiguos.
+var AUTOPREFIXER_BROWSERS = [
+	'ie >= 10',
+	'ie_mob >= 10',
+	'ff >= 30',
+	'chrome >= 34',
+	'safari >= 7',
+	'opera >= 23',
+	'ios >= 7',
+	'android >= 4.4',
+	'bb >= 10'
+];
--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,40 @@
 'use strict';
 
 const gulp = require('gulp');
+const runSequence = require('run-sequence');
 const plumber = require('gulp-plumber');
 const notify = require("gulp-notify");
-const argv = require('yargs').argv;
+const gutil = require('gulp-util');
+const file = require('gulp-file');
+const filter = require('gulp-filter');
+const argv = require('yargs').default('production', false).argv;
 const gulpif = require('gulp-if');
 const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
 const jade = require('gulp-jade');
 const less = require('gulp-less');
 const autoprefixer = require('gulp-autoprefixer');
 const minifyCSS = require('gulp-minify-css');
 const browserSync = require('browser-sync').create();
+const path = require('path');
+const swPrecache = require('sw-precache');
 const del = require('del');
+const gzip = require('gulp-gzip');
 
-const JADE_PATH = 'src/jade/*.jade';
-const LESS_PATH = 'src/less/**/*.less';
-const LESS_ENTRY = 'src/less/nacar.less';
-const IMG_PATH = 'src/img/**/*';
+const SRC_DIR = 'src';
+const DEST_PATH = 'public';
+
+const JADE_PATH = path.join(SRC_DIR, 'jade/*.jade')
+const LESS_PATH = path.join(SRC_DIR, 'less/**/*.less');
+const LESS_ENTRY = path.join(SRC_DIR, 'less/nacar.less');
+const IMG_PATH = path.join(SRC_DIR, 'img/**/*');
+const JS_PATH = path.join(SRC_DIR, '**/*.js');
 
 gulp.task('jade', function () {
 	return gulp.src(JADE_PATH)
 		.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
 		.pipe(jade({pretty: !argv.production}))
-		.pipe(gulp.dest('./public'))
+		.pipe(gulp.dest(DEST_PATH))
 		.pipe(browserSync.stream({once: true}));
 });
 
@@ -36,26 +48,66 @@ gulp.task('less', function () {
 			cascade: false
 		})).pipe(gulpif(argv.production, minifyCSS()))
 		.pipe(gulpif(!argv.production, sourcemaps.write()))
-		.pipe(gulp.dest('./public'))
-		.pipe(browserSync.stream());
+		.pipe(gulp.dest(DEST_PATH))
+		.pipe(browserSync.stream())
+		.pipe(gulpif(argv.production, gzip()))
+		.pipe(gulpif(argv.production, gulp.dest(DEST_PATH)));
 });
 
 gulp.task('img', function () {
 	return gulp.src(IMG_PATH)
-		.pipe(gulp.dest('./public/img'))
-		.pipe(browserSync.stream());
+		.pipe(gulp.dest(path.join(DEST_PATH, 'img')))
+		.pipe(browserSync.stream())
+		.pipe(filter(['**/*.svg']))
+		.pipe(gulpif(argv.production, gzip()))
+		.pipe(gulpif(argv.production, gulp.dest(path.join(DEST_PATH, 'img'))));
 });
+
+gulp.task('js', function () {
+	return gulp.src(JS_PATH)
+		.pipe(gulpif(argv.production, uglify()))
+		.pipe(gulp.dest(DEST_PATH))
+		.pipe(browserSync.stream())
+		.pipe(gulpif(argv.production, gzip()))
+		.pipe(gulpif(argv.production, gulp.dest(DEST_PATH)));
+});
+
+gulp.task('build-files', ['jade', 'less', 'img', 'js']);
 
 gulp.task('clean', function () {
-	return del("public");
+	return del(path.join(DEST_PATH, '**/*'));
 });
 
-gulp.task('build', ['jade', 'less', 'img']);
+gulp.task('generate-service-worker:clean', function() {
+	return del(path.join(DEST_PATH, 'service-worker.js'));
+});
+
+gulp.task('generate-service-worker', ['generate-service-worker:clean'], function() {
+	return swPrecache.generate({
+		cacheId: 'nacar',
+		staticFileGlobs: [path.join(DEST_PATH, '**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}')],
+		stripPrefix: DEST_PATH,
+		//dontCacheBustUrlsMatching: /./,
+		handleFetch: argv.production,
+		logger: gutil.log,
+	}).then((serviceWorkerString) => {
+		return file('service-worker.js', serviceWorkerString, {src: true})
+			.pipe(gulpif(argv.production, uglify()))
+			.pipe(gulp.dest(DEST_PATH))
+			.pipe(gulpif(argv.production, gzip()))
+			.pipe(gulpif(argv.production, gulp.dest(DEST_PATH)));
+	});
+});
+
+gulp.task('build', function(callback) {
+	runSequence('build-files', 'generate-service-worker', callback);
+});
 
 gulp.task('watch', ['build'], function () {
-	gulp.watch('src/jade/**/*.jade', ['jade']);
-	gulp.watch(LESS_PATH, ['less']);
-	gulp.watch(IMG_PATH, ['img']);
+	gulp.watch(JADE_PATH, ['jade', 'generate-service-worker']);
+	gulp.watch(LESS_PATH, ['less', 'generate-service-worker']);
+	gulp.watch(IMG_PATH, ['img', 'generate-service-worker']);
+	gulp.watch(JS_PATH, ['js', 'generate-service-worker']);
 });
 
 gulp.task('serve', ['watch'], function() {
@@ -64,4 +116,6 @@ gulp.task('serve', ['watch'], function() {
 	});
 });
 
-gulp.task('default', ['build']);
+gulp.task('default', function(callback) {
+	runSequence('clean', 'build', callback);
+});
